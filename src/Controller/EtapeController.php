ec471a05d9ed81f1d0b964fc7d949e88c16549cb
azy64 <allysaidi64@gmail.com>
2022-05-11 17:59:55 +0200

    Add crud for Etape

--- src/Controller/EtapeController.php
+++ src/Controller/EtapeController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Etape;
+use App\Entity\Voyages;
 use App\Form\EtapeType;
 use App\Repository\EtapeRepository;
+use App\Repository\TransportRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,9 +35,10 @@ class EtapeController extends AbstractController
     /**
      * @Route("/new", name="app_etape_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, ManagerRegistry $entityManager): Response
+    public function new(Request $request): Response
     {
         $etape = new Etape();
+        $entityManager = $this->getDoctrine()->getManager();
         $form = $this->createForm(EtapeType::class, $etape);
         $form->handleRequest($request);
 
@@ -43,7 +46,8 @@ class EtapeController extends AbstractController
             $entityManager->persist($etape);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_etape_index', [], Response::HTTP_SEE_OTHER);
+            //return $this->redirectToRoute('app_etape_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_voyages_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('etape/new.html.twig', [
@@ -65,8 +69,9 @@ class EtapeController extends AbstractController
     /**
      * @Route("/{id}/edit", name="app_etape_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Etape $etape, ManagerRegistry $entityManager): Response
+    public function edit(Request $request, Etape $etape): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
         $form = $this->createForm(EtapeType::class, $etape);
         $form->handleRequest($request);
 
@@ -85,13 +90,47 @@ class EtapeController extends AbstractController
     /**
      * @Route("/{id}", name="app_etape_delete", methods={"POST"})
      */
-    public function delete(Request $request, Etape $etape, ManagerRegistry $entityManager): Response
+    public function delete(Request $request, Etape $etape): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
         if ($this->isCsrfTokenValid('delete'.$etape->getId(), $request->request->get('_token'))) {
             $entityManager->remove($etape);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('app_etape_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    /**
+     * @Route("/add-etape/{id}", name="add_etape", methods={"GET", "POST"})
+     */
+    public function addEtape(Request $request, Voyages $voyage,TransportRepository $trans, EtapeRepository $rep): Response{
+
+        $etape = new Etape();
+        $etape->setVoyage($voyage);
+        $form = $this->createForm(EtapeType::class, $etape);
+        $form->handleRequest($request);
+        $entityManager = $this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tmp = $rep->findBy(['voyage' => $etape->getVoyage(),'depart'=>$etape->getDepart()]);
+            $tmp1 = $rep->findBy(['voyage' => $etape->getVoyage(),'arrival'=>$etape->getArrival()]);
+            if($tmp == null && $tmp1 == null){
+                $entityManager->persist($etape);
+                $entityManager->flush();
+                return $this->redirectToRoute('app_voyages_index', [], Response::HTTP_SEE_OTHER);
+            }
+
+            //return $this->redirectToRoute('app_etape_index', [], Response::HTTP_SEE_OTHER);
+            return $this->renderForm('etape/new.html.twig', [
+                'etape' => $etape,
+                'form' => $form,
+            ]);
+        }
+
+        return $this->renderForm('etape/new.html.twig', [
+            'etape' => $etape,
+            'form' => $form,
+        ]);
     }
 }
