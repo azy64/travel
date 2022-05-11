f66c46f779ff7161c1d35edb469a1a3f01ebd3ab
azy64 <allysaidi64@gmail.com>
2022-05-11 18:29:48 +0200

    Install phpunit

---
+++ tests/EtapeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class EtapeTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
